156d58b47960cccb90b47a992d79896c3978aae9
Naseem Qurbanli <nq222af@student.lnu.se>
2025-06-05 10:21:56 +0200

    some funcition upgraded or edited

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ This App was built with purpose to help concentrate while doing hobby projects o
 # For styling and responsive desing - CSS
 # For logic - JavaScript
 
-Although this app is built only local space so to use it follow the instructions.
+Although this app is built only in local environment so to use it follow the instructions.
 Step1: Install any code editor followed by installing any liveServer compitiable with your code editor.
 Step 2: use cmd/bash/VSCode terminal to git clone the repo.
 Step 3: After cloning Cd first to Timer directory & to public directory.
--- public/script.js
+++ public/script.js
@@ -1,14 +1,22 @@
 /* Authour : Naseem Qurbanali */
 
 let startTime = 0;
+let elapsed = 0;
 let timmerInterval = null;
 let isRunning = false;
+let isPomodor = false;
+let pomodorMode = "work";
 
+
+const work_duration = 25 * 60 * 1000;
+const break_duration = 5 * 60 * 1000;
 const timerDisplay = document.getElementById('timer');
 const startbtn = document.getElementById('start');
 const pausebtn = document.getElementById('pause');
 const resetbtn = document.getElementById('reset');
 const modeToggle = document.getElementById('modeToggle');
+const pomodorBtn = document.getElementById("pomodoro");
+
 
 /* Time formater*/
 function formatTime(ms) {
@@ -24,27 +32,71 @@ function updateDisplay() {
     timerDisplay.textContent = formatTime(elapsed);
 }
 
+function startTimer( duration = null) {
+    startTime = Date.now() - elapsed;
+    timmerInterval = setInterval(() => {
+        elapsed = Date.now() - startTime;
+        if (isPomodor) {
+            const limit = pomodorMode === "work" ? work_duration : break_duration;
+            if (elapsed >= limit) {
+                clearInterval(timmerInterval);
+                isRunning = false;
+                alert(pomodorMode === "work" ? "Time for a break!" : "Back to work!");
+                switchPomodoroMode();
+                return;
+            }
+        }
+        timerDisplay.textContent = formatTime(elapsed);
+    }, 1000);
+    isRunning = true;
+    pausebtn.innerHTML = "Pause";
+}
+
+function switchPomodoroMode () {
+    elapsed = 0;
+    pomodorMode = pomodorMode === "work" ? "break" : "work";
+    startTimer();
+}
+
+function pauseTimer() {
+    clearInterval(timmerInterval);
+    isRunning = false;
+    pausebtn.innerHTML = "Unpause";
+}
+
 startbtn.onclick = () => {
-    if (!isRunning) {
-        startTime = Date.now() -( timmerInterval ? Date.now() - starTime : 0);
-        timmerInterval = setInterval(updateDisplay, 1000);
-        isRunning = true;
+    if (!isRunning && elapsed === 0) {
+        startTimer();
     }
 }
 
 pausebtn.onclick = () => {
-    clearInterval(timmerInterval);
-    timmerInterval = null;
-    isRunning = false;
+    if (isRunning) {
+        pauseTimer();
+    } else {
+        startTimer();
+    }
 }
 
-resetbtn.onclick = () => {
+
+function resetTimer() {
     clearInterval(timmerInterval);
     timmerInterval = null;
-    isRunning = false;
     startTime = 0;
-    timerDisplay,textContent = "00:00:00"
+    elapsed = 0;
+    isRunning= false;
+    timerDisplay.innerHTML = "00:00:00";
+    pausebtn.innerHTML = "Pause";
+    if (isPomodor) pomodorMode = "work";
 }
+
+resetbtn.onclick = resetTimer;
+
+pomodorBtn.onclick = () => {
+    resetTimer();
+    isPomodor = !isPomodor;
+    pomodorBtn.innerHTML = isPomodor ? "Pomodor: On" : "Pomodor: OFF";
+};
 
 modeToggle.onchange = () =>  {
     document.body.classList.toggle('dark', modeToggle.checked);
